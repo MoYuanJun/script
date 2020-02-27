63cc00d53ae1ac415ed69e2e3f8efe2da8d29271
qianyin925 <qianyin95@gmail.com>
2020-02-27 09:55:38 +0800

    feat: 脚本 deploy 新增配置项

--- script/deploy/choices.js
+++ script/deploy/choices.js
@@ -35,5 +35,11 @@ module.exports = [
     ip: '172.20.54.119',
     name: '集成框架(54.119)',
     path: '/opt/sgg/web/apache-tomcat-8.5.32/webapps/ROOT',
-  }
+  },
+  {
+    user: 'root',
+    ip: '172.20.54.142',
+    name: 'BSC 销可版本(54.142)',
+    path: '/opt/sailing/web/apache-tomcat-8.5.32/webapps/ROOT',
+  },
 ];
