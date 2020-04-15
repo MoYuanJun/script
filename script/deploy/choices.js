4d27be3cba01efe0d746b8861a26cc41a120219e
qianyin925 <qianyin95@gmail.com>
2020-04-15 09:14:09 +0800

    feat: 部署脚本部署脚本更新

--- script/deploy/choices.js
+++ script/deploy/choices.js
@@ -1,24 +1,9 @@
+const shell = require('shelljs');
+
+const company = shell.find('~/Company/ziliao')
+  .filter(file => file.match(/put\.js$/))
+  .map(file => require(file));
+
 module.exports = [
-  {
-    user: 'root',
-    port: 40000,
-    // password: '可以配置密码, 这样就不用手动输入了',
-    ip: '172.20.54.119',
-    name: '集成框架(54.119)',
-    path: '/opt/sgg/web/apache-tomcat-8.5.32/webapps',
-  },
-  {
-    user: 'root',
-    port: 22,
-    ip: '172.20.54.141',
-    name: 'BSC(54.141)',
-    path: '/opt/sailing/web/apache-tomcat-8.5.32/webapps',
-  },
-  {
-    user: 'root',
-    port: 22,
-    ip: '172.20.54.140',
-    name: 'SCMS(54.140)',
-    path: '/opt/sailing/web/apache-tomcat-8.5.32/webapps',
-  }
+  ... company,
 ];
