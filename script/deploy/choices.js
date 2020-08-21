93ec1f420edf29759b4a2c501fae6bb3fe92ec05
qianyin925 <qianyin925@qq.com>
2020-08-21 14:54:16 +0800

    refactor: 项目部署脚本修改

--- script/deploy/choices.js
+++ script/deploy/choices.js
@@ -1,6 +1,6 @@
 const shell = require('shelljs');
 
-const companyPath = shell.find('~/Company/ziliao/put.js')[0];
+const companyPath = shell.find('~/company/ziliao/put.js')[0];
 const company = companyPath ?  require(companyPath) : [];
 
 module.exports = [
